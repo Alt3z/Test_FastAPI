51a024ac3eaf745dc25b468d080c40980e54917b
Altez <xxx>
2025-04-07 23:13:07 +0500

    jwt v.1

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -4,3 +4,7 @@ class RegUser(BaseModel):
     #id: int = Field(description="ID пользователя")
     user_name: str = Field(min_length=4, max_length=25, description="Имя пользователя")
     password: str = Field(min_length=8, max_length=32, description="Пароль пользователя")
+
+class LogUser(BaseModel):
+    user_name: str = Field(min_length=4, max_length=25, description="Имя пользователя")
+    password: str = Field(min_length=8, max_length=32, description="Пароль пользователя")
